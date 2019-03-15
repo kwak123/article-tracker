5a585c4b90903b061a66ef76dea9398cd8538776
kwak123 <kwak123@gmail.com>
2019-03-15 12:05:10 -0400

    add eslint-standard (but with trailing commas)

--- server/util/index.js
+++ server/util/index.js
@@ -1,10 +1,10 @@
 // Helpers here
 
 const logger = (req, res, next) => {
-  console.log(`${req.method} request received at ${req.url}`);
-  next();
-};
+  console.log(`${req.method} request received at ${req.url}`)
+  next()
+}
 
 module.exports = {
-  logger
-};
+  logger,
+}
